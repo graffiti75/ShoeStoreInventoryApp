129b2ac4aa29256456b3b488beb6bcb33a3ff4d9
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-01-16 19:51:05 -0500

    Navigation xml file created - Login screen created

--- app/src/main/java/com/graffiti75/shoestore/LoginActivity.kt
+++ app/src/main/java/com/graffiti75/shoestore/LoginActivity.kt
@@ -3,9 +3,12 @@ package com.graffiti75.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+/**
+ * https://www.section.io/engineering-education/how-is-textinputlayout-different-from-edittext/
+ */
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
